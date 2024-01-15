13ba5b7c601999937e5bafa68fa1f6502554da0e
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2024-01-15 13:23:31 -0500

    dfh

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: sophie
+ * Created on: Jan 2024
+ * This program turns bottle caps.
 */
 
-basic.showString('Hello, World!')
+
+// variables
+let distance: number = 0
+
+// setup
+basic.clearScreen()
+radio.setGroup(5)
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+// receive message
+radio.onReceivedString(function (receivedString) {
+  basic.clearScreen()
+  basic.showString(receivedString)
+  basic.showIcon(IconNames.Happy)
+
+  // spin broom thingy
+  robotbit.StpCarMove(-10, 48)
+  basic.pause(500)
+  robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+  basic.pause(500)
+  robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+  basic.pause(500)
+  robotbit.StpCarMove(10, 48)
+
+  //sonar - edge detection
+  distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters) 
+})
